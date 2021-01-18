dacdde5413edb498eee9e02d9fb46818091c6bde
Tony <tony,wu@baoxitech.com>
2021-01-18 16:26:03 +0800

    'init'

---
+++ src/test/java/im/tao/eurekacenter/EurekaCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package im.tao.eurekacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
